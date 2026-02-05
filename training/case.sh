c606e7a4c212971e0693580703482ae0cc9d9030
mathist <mathist@mymail.com>
2026-02-05 20:15:56 +0100

    Case

---
+++ training/case.sh
@@ -0,0 +1,16 @@
+s=$1
+
+case "$s" in 
+	dave)
+		echo "hi Dave"
+		;;
+	buddy)
+		echo "Oh here he is"
+		;;
+	guy)
+		echo "Here comes trouble"
+		;;
+	*)
+		echo "I don't know who you are"
+		;;
+esac 
