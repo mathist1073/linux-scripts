d5e3407382c93542d5a6499dd55d92ff5bd467f7
mathist <mathist@mymail.com>
2026-03-11 19:23:44 +0100

    sectin 5

--- training/process_substitution.sh
+++ training/process_substitution.sh
@@ -16,6 +16,7 @@ y=0
 while read -r word2; do
   echo "$word2"
   ((y++))
+  # < expect a file (redirector) and thr sub process <(...) for example here provides a file
 done < <(grep d /usr/share/dict/cracklib-small)
 
 echo "found $y words"
